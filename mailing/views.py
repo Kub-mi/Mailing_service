197ed657efb1a41482cc42819b64e6e1b265d084
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-08-25 06:09:57 +0300

    изменил модели для ограниченного доступа к ним

--- mailing/views.py
+++ mailing/views.py
@@ -6,18 +6,43 @@ from .models import Client
 from .forms import ClientForm
 
 
+PERM_VIEW_ALL = "mailing.view_all_clients"
+
 class ClientListView(LoginRequiredMixin, ListView):
     model = Client
-    template_name = 'mailing/client.html'
+    template_name = 'mailing/client_list.html'
     context_object_name = 'clients'
     paginate_by = 10
-    manager_perm = 'mailing.views_all_clients'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.has_perm(PERM_VIEW_ALL):
+            return qs
+        return qs.filter(owner=self.request.user)
 
 
 class ClientDetailView(LoginRequiredMixin, DetailView):
     model = Client
     template_name = 'mailing/client_detail.html'
-    manager_perm = 'mailing.views_all_clients'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.has_perm(PERM_VIEW_ALL):
+            return qs
+        return qs.filter(owner=self.request.user)
+
+
+class ClientCreateView(LoginRequiredMixin, CreateView):
+    model = Client
+    form_class = ClientForm
+    template_name = "mailing/client_form.html"
+    success_url = reverse_lazy("mailing:client_list")
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.owner = self.request.user
+        obj.save()
+        return super().form_valid(form)
 
 
 class ClientUpdateView(LoginRequiredMixin, UpdateView):
@@ -25,12 +50,22 @@ class ClientUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ClientForm
     template_name = 'mailing/client_form.html'
     success_url = reverse_lazy('mailing:client_list')
-    manager_perm = 'mailing.views_all_clients'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.has_perm(PERM_VIEW_ALL):
+            return qs
+        return qs.filter(owner=self.request.user)
 
 
 class ClientDeliteView(LoginRequiredMixin, DeleteView):
     model = Client
     template_name = 'mailing/confirm_delete.html'
     success_url = reverse_lazy('mailing:client_list')
-    manager_perm = 'mailing.views_all_clients'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.has_perm(PERM_VIEW_ALL):
+            return qs
+        return qs.filter(owner=self.request.user)
 
