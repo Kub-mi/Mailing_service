0ae779b86f9dbdae6848b67b600918618dc427c2
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-08-25 05:42:41 +0300

    добавил views для клиента

---
+++ mailing/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    ClientListView, ClientDetailView, ClientCreateView,
+    ClientUpdateView, ClientDeleteView
+)
+
+app_name = "mailing"
+
+urlpatterns = [
+    # Clients
+    path("clients/", ClientListView.as_view(), name="client_list"),
+    path("clients/create/", ClientCreateView.as_view(), name="client_create"),
+    path("clients/<int:pk>/", ClientDetailView.as_view(), name="client_detail"),
+    path("clients/<int:pk>/update/", ClientUpdateView.as_view(), name="client_update"),
+    path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="client_delete"),
+]
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Client
+from .forms import ClientForm
+
+
+class ClientListView(LoginRequiredMixin, ListView):
+    model = Client
+    template_name = 'mailing/client.html'
+    context_object_name = 'clients'
+    paginate_by = 10
+    manager_perm = 'mailing.views_all_clients'
+
+
+class ClientDetailView(LoginRequiredMixin, DetailView):
+    model = Client
+    template_name = 'mailing/client_detail.html'
+    manager_perm = 'mailing.views_all_clients'
+
+
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Client
+    form_class = ClientForm
+    template_name = 'mailing/client_form.html'
+    success_url = reverse_lazy('mailing:client_list')
+    manager_perm = 'mailing.views_all_clients'
+
+
+class ClientDeliteView(LoginRequiredMixin, DetailView):
+    model = Client
+    template_name = 'mailing/confirm_delete.html'
+    success_url = reverse_lazy('mailing:client_list')
+    manager_perm = 'mailing.views_all_clients'
+
