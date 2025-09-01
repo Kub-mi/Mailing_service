0ace923dba51ab7a282fb9a8e8edbf3f36f5c673
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-01 07:58:22 +0300

    добавил кастомного пользователя и миграции

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "username", "is_active", "is_staff")
+    ordering = ("email",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Дополнительно", {"fields": ("phone", "avatar", "country")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone = models.CharField(max_length=15, blank=True, null=True, verbose_name="Телефон")
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, verbose_name="Аватар")
+    country = models.CharField(max_length=64, blank=True, verbose_name="Страна")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]  # username просит Django для AbstractUser
+
+    def __str__(self):
+        # чтобы в админке и логах был наглядный вывод
+        return self.email or self.username
