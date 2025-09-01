cedaa9671e6d924254924a69cb4322db3db4be48
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-01 09:14:28 +0300

    добавил модель админку  для рассыло

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Client, Message
+from .models import Client, Message, Mailing
 
 
 @admin.register(Client)
@@ -14,3 +14,11 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ("subject", "owner")
     search_fields = ("subject", "body", "owner__email")
     list_filter = ("owner",)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("id", "status", "start_at", "finish_at", "message", "owner")
+    list_filter = ("status", "owner")
+    search_fields = ("id", "message__subject", "owner__email")
+    filter_horizontal = ("recipients",)
