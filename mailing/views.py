88c570b30a3283cdd714408ed18069908679bdac
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-01 06:29:10 +0300

    добавил модель vews для сообщений

--- mailing/views.py
+++ mailing/views.py
@@ -2,11 +2,12 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Client
-from .forms import ClientForm
+from .models import Client, Message
+from .forms import ClientForm, MessageForm
 
 
 PERM_VIEW_ALL = "mailing.view_all_clients"
+PERM_VIEW_ALL_MESSAGES = "mailing.view_all_messages"
 
 class ClientListView(LoginRequiredMixin, ListView):
     model = Client
@@ -69,3 +70,63 @@ class ClientDeliteView(LoginRequiredMixin, DeleteView):
             return qs
         return qs.filter(owner=self.request.user)
 
+
+class MessageListView(LoginRequiredMixin, ListView):
+    model = Message
+    template_name = 'mailing/message_list.html'
+    context_object_name = 'message'
+    paginate_by = 10
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.has_perm(PERM_VIEW_ALL_MESSAGES):
+            return qs
+        return qs.filter(owner=self.request.user)
+
+
+class MessageDetailView(LoginRequiredMixin, DetailView):
+    model = Message
+    template_name = 'mailing/message_detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.has_perm(PERM_VIEW_ALL_MESSAGES):
+            return qs
+        return qs.filter(owner=self.request.user)
+
+
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    model = Message
+    form_class = MessageForm
+    template_name = 'mailing/message_form.html'
+    success_url = reverse_lazy('mailing:message_list')
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.owner = self.request.user
+        obj.save()
+        return super().form_valid(form)
+
+class MessageUpdateView(LoginRequiredMixin, UpdateView):
+    model = Message
+    form_class = MessageForm
+    template_name = 'mailing/message_form.html'
+    success_url = reverse_lazy('mailing:message_list')
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.has_perm(PERM_VIEW_ALL_MESSAGES):
+            return qs
+        return qs.filter(owner=self.request.user)
+
+
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
+    model = Message
+    template_name = 'mailing/message_delete.html'
+    success_url = reverse_lazy('mailing:message_list')
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.has_perm(PERM_VIEW_ALL_MESSAGES):
+            return qs
+        return qs.filter(owner=self.request.user)
