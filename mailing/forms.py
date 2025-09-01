ef5760b1c33adf9f9d5772ce19e0e729b34cdbb0
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-01 09:06:27 +0300

    добавил форму  рассылки

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Client, Message
+from .models import Client, Message, Mailing
 
 
 class ClientForm(forms.ModelForm):
@@ -12,3 +12,21 @@ class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ["subject", "body"]
+
+
+class MailingForm(forms.ModelForm):
+    class Meta:
+        model = Mailing
+        fields = ["start_at", "finish_at", "status", "message", "recipients"]
+        widgets = {
+            "start_at": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "finish_at": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        }
+
+        def clean(self):
+            data = super().clean()
+            start_at = data.get("start_at")
+            finish_at = data.get("finish_at")
+            if start_at and finish_at and finish_at <= start_at:
+                raise forms.ValidationError("Дата окончания должна быть позже даты начала.")
+            return data
