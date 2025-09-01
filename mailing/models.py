c86408fc44c038723f10cd6a035822a2b2db6c59
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-01 06:11:17 +0300

    добавил модель сообщения

--- mailing/models.py
+++ mailing/models.py
@@ -17,3 +17,19 @@ class Client(models.Model):
 
     def __str__(self):
         return f'{self.full_name} <{self.email}>'
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=255, verbose_name='Тема письма')
+    body = models.TextField(verbose_name='Тело письма')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='messages', verbose_name='Владелец')
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
+        permissions = [
+            ('view_all_messages', 'Can view all messages (manager)'),
+        ]
+
+    def __str__(self):
+        return self.subject
