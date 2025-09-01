454462036fb28693ec6beb67b253d43d266d1834
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-01 06:13:04 +0300

    добавил модель form

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Client
+from .models import Client, Message
 
 
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ["email", "full_name", "comment"]
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ["subject", "body"]
