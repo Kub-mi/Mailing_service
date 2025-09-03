86f210032fee2c579af774d2473d690e3f9fc211
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-03 08:08:44 +0300

    добавил логин, домашнюю страницу и исправил шаблоны

--- config/urls.py
+++ config/urls.py
@@ -21,8 +21,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("pages.urls", namespace="pages")),
     path("mailing/", include("mailing.urls", namespace="mailing")),
+    path("users/", include("users.urls", namespace="users")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
